f410833c2f17c3e2f08320e2337c3bde7c3372bf
lrazo-testlio <luis-testlio@screencast-o-matic.com>
2026-03-27 03:27:29 +0100

    Initial project creation and work

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Header from "./components/Header"
+import Home from './components/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>      
+    
+  )
+}
+
+export default App
