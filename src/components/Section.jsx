bc3f2862a72e0437b285d074b240e31d348d47c0
lrazo-testlio <luis-testlio@screencast-o-matic.com>
2026-03-28 05:07:25 +0100

    Adding different sections

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import styled from "styled-components"
 
-function Section() {
+function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
   return (
-    <Wrap>
+      <Wrap bgImage={ backgroundImg }>
         <ItemText>
-            <h1>Model S</h1>
-            <p>Order Online for Touchless Delivery</p>
+            <h1>{ title }</h1>
+            <p>{ description }</p>
         </ItemText>
         <Buttons>
               <ButtonGroup>
                   <LeftButton>
-                      Custom Order
+                      { leftBtnText }
                   </LeftButton>
                   <RightButton>
-                      Existing Inventory
+                      { rightBtnText }
                   </RightButton>
               </ButtonGroup>
               <DownArrow src="/images/down-arrow.svg" />
@@ -36,7 +36,8 @@ const Wrap = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between; //vertical
-    align-items: center;
+    align-items: center; 
+    background-image: ${ props => `url("/images/${props.bgImage}")` }
 `
 
 const ItemText = styled.div`
@@ -47,6 +48,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
     display: flex;
     margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `
 
 const LeftButton = styled.div`
@@ -66,7 +70,9 @@ const LeftButton = styled.div`
 `
 
 const RightButton = styled(LeftButton)`
-
+    background: white;
+    opacity: 0.65;
+    color: black;
 `
 
 const DownArrow = styled.img`
